4a86888491769fe150d57928736d1be9fd5cb861
Andrew Talanov <andrewtalanov@mail.ru>
2022-06-12 21:50:44 +0500

    контент главной страницы, заготовка смены темы

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,48 @@ import "../scss/style.scss";
 
 import * as flsFunctions from "./files/functions.js";
 
-flsFunctions.isWebp();
+const btnToggleTheme = document.querySelector('.btn-toggle-theme')
+
+const root = document.documentElement;
+const backgroundDarkTheme = document.querySelector('.dark-theme');
+
+const colorWhite = '#fff';
+const colorBlack = '#222329';
+
+if (window.localStorage.getItem('theme') === null) {
+  window.localStorage.setItem('theme', 1);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  flsFunctions.isWebp();
 
 
-// import "./files/sliders.js";
+  // import "./files/sliders.js";
+
+  toggleTheme();
+
+  btnToggleTheme.addEventListener('click', () => {
+    if (window.localStorage.getItem('theme') == 1) {
+      window.localStorage.setItem('theme', 0);
+    } else {
+      window.localStorage.setItem('theme', 1);
+    }
+    toggleTheme();
+  });
+
+  function toggleTheme() {
+    if (window.localStorage.getItem('theme') == 1) {
+      root.style.setProperty('--colorFonts', colorBlack);
+      backgroundDarkTheme.style.opacity = 0;
+    } else {
+      root.style.setProperty('--colorFonts', colorWhite);
+      backgroundDarkTheme.style.opacity = 1;
+    }
+
+  };
+
+
+
+});
+
