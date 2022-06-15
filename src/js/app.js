311d4e072a0cc1d01fb20ea4c6e7609efb050b7a
Andrew Talanov <andrewtalanov@mail.ru>
2022-06-15 12:16:23 +0500

    заготовка под слайдер, осталась стилизация

--- src/js/app.js
+++ src/js/app.js
@@ -30,10 +30,6 @@ var swiper = new Swiper(".js-mySwiper", {
   slidesPerGroup: 3,
   loop: true,
   loopFillGroupWithBlank: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
