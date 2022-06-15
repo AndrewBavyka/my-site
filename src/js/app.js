bf34e0a8aa4608b8ee70cc3e78f9df58eed186c2
Andrew Talanov <andrewtalanov@mail.ru>
2022-06-15 10:43:45 +0500

    заготовка под слайдер, осталась стилизация

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,10 @@
 "use strict";
 
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
 import "../scss/style.scss";
 
 // изменение цвета темы
@@ -14,10 +19,26 @@ import "./files/animationBtnOpenMenu.js";
 // Переключение экранов
 import "./files/toggleTabs.js";
 
-// import "./files/sliders.js";
 import * as flsFunctions from "./files/functions.js";
 
 flsFunctions.isWebp();
+
+
+var swiper = new Swiper(".js-mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 
 
