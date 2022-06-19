ba458c46e19e20e2bf904e6e6b94d78e3bcf9480
Andrew Talanov <andrewtalanov@mail.ru>
2022-06-19 13:53:19 +0500

    ..

--- src/js/files/toggleTabs.js
+++ src/js/files/toggleTabs.js
@@ -1,10 +1,13 @@
 const tabs = document.querySelectorAll('.js-page');
 const btnToggleTabs = document.querySelectorAll('.js-btn-tab');
+const btnTabsImg = document.querySelectorAll('.js-btn-tab img');
+
+const root = document.documentElement;
 
 (function verifyTabs() {
   if (sessionStorage.getItem('active-page') == null) {
     sessionStorage.setItem('active-page', 0);
-    toggleTabs(0);
+    toggleTabs(sessionStorage.getItem('active-page'));
   } else {
     toggleTabs(sessionStorage.getItem('active-page'));
   }
@@ -13,13 +16,29 @@ const btnToggleTabs = document.querySelectorAll('.js-btn-tab');
 
 btnToggleTabs.forEach((item, id) => {
   item.addEventListener('click', () => {
-    toggleTabs(id)
     sessionStorage.setItem('active-page', id);
+    toggleTabs(sessionStorage.getItem('active-page'));
   })
 })
 
 
 function toggleTabs(id) {
+
+  btnTabsImg.forEach(item => {
+    // черный цвет иконки
+    item.style.filter = 'invert(0%) sepia(97%) saturate(0%) hue-rotate(188deg) brightness(99%) contrast(104%)';
+  });
+  // белый цвет иконки
+  btnTabsImg[id].style.filter = 'invert(100%) sepia(0%) saturate(0%) hue-rotate(323deg) brightness(106%) contrast(103%)';
+  
+  btnToggleTabs.forEach(item => {
+    // серый цвет кнопки
+    item.style.backgroundColor = 'var(--lightGray)';
+  });
+  // фиолетовый цвет кнопки
+  btnToggleTabs[id].style.backgroundColor = 'var(--violet)';
+  
+
   tabs.forEach(item => {
     item.style.display = 'none';
   });
